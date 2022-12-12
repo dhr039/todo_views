40486c7654828ea59e09d2683002efd6d471d368
Dmitry Hristov <dumitru.hristov@muume.com>
2022-12-12 16:51:37 +0200

    inverting our dependencies;

---
+++ app/src/main/java/com/commonsware/todo_3/ToDoApp.kt
@@ -0,0 +1,21 @@
+package com.commonsware.todo_3
+
+import android.app.Application
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class ToDoApp : Application() {
+    private val koinModule = module {
+        single { ToDoRepository() }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            modules(koinModule)
+        }
+    }
+}
